9771edf9acccc622bd41fefcad33b031911b7051
luxumbra <me@luxumbra.dev>
2024-08-08 04:21:39 +0100

    responsive tweaks

--- src/scripts/birthday.js
+++ src/scripts/birthday.js
@@ -1,10 +1,12 @@
 document.addEventListener('astro:page-load', () => {
   const birthdayEl = document.getElementById('birthday-message');
   if (birthdayEl) {
+    document.body.style.overflow = 'hidden';
     const button = birthdayEl.querySelector('button');
     button.addEventListener('click', () => {
       if (birthdayEl.classList.contains('flex')) {
         birthdayEl.style.opacity = '0'
+        document.body.style.overflow = 'visible'
         setTimeout(() => {
           birthdayEl.style.display = 'none';
         }, 1000);
